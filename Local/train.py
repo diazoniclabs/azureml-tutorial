6ec6f1dd30b8c13a6bfabf71629769efa13a3da7
Diazonic Labs <61781809+diazoniclabs@users.noreply.github.com>
2022-06-18 00:47:15 +0530

    Update train.py

--- Local/train.py
+++ Local/train.py
@@ -4,8 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 sal = pd.read_csv('data/sal.csv',header=0, index_col=None)
-X = sal[['x']]
-y = sal['y']
+X = sal[['YearsExperience']]
+y = sal['Salary']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=10)
 
@@ -20,6 +20,6 @@ y_predict= lm.predict(X_test)
 mse = mean_squared_error(y_predict,y_test)
 print('MSE :', round(mse,2))
 
-from sklearn import joblib
+import joblib
 filename = 'model/sal_model.pkl'
 joblib.dump(lm, filename)
